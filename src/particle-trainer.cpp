f569b7759d6099fbb77ea609038259eb626d32ea
Maurik Holtrop <maurik@physics.unh.edu>
2021-07-12 16:05:50 -0400

    Add gun for particle training.

--- src/particle-trainer.cpp
+++ src/particle-trainer.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
 
     setlocale(LC_NUMERIC, "");
     cxxopts::Options options(argv[0], ". This program will send n"
-                                      " particles of type p to a stdhep file.");
+                                      " particles of different types to a stdhep file.");
     options
             .positional_help(" outfile - Output file.")
             .show_positional_help();
@@ -39,14 +39,15 @@ int main(int argc, char **argv) {
     options
             .add_options()
                     ("d,debug", "Set debug level", cxxopts::value<int>()->default_value("0"))
-                    ("p,part",  "Set particle type [default: 11]", cxxopts::value<int>()->default_value("11"))
-                    ("m,mass",  "Set particle mass [default: 0.000511]", cxxopts::value<double>()->default_value("0.000511"))
-                    ("min",  "Set minimum momentum [default: 0.1]", cxxopts::value<double>()->default_value("0.1"))
-                    ("max",  "Set maximum momentum [default: 2.3]", cxxopts::value<double>()->default_value("2.3"))
+                    ("p,part",  "Set particle type mix [default: 1]", cxxopts::value<int>()->default_value("1"))
+                    ("nmax",  "Set max number of particles per event [default: 1]", cxxopts::value<int>()->default_value("1"))
+                    ("pmin",  "Set minimum momentum [default: 0.1]", cxxopts::value<double>()->default_value("0.01"))
+                    ("pmax",  "Set maximum momentum [default: 2.3]", cxxopts::value<double>()->default_value("4.6"))
                     ("xmin",  "Set minimum x at 150cm (Ecal) [default: -42.]", cxxopts::value<double>()->default_value("-42."))
                     ("xmax",  "Set maximum x at 150cm (Ecal) [default: 42.]", cxxopts::value<double>()->default_value("42."))
                     ("ymin",  "Set minimum y at 150cm (Ecal) [default: 1.]", cxxopts::value<double>()->default_value("1."))
                     ("ymax",  "Set maximum y at 150cm (Ecal) [default: 10.]", cxxopts::value<double>()->default_value("10."))
+                    ("rot",   "Set beam rotation [default: 32.5 mrad]", cxxopts::value<double>()->default_value("32.5"))
 
                     ("o,outputfile", "Output files (-o is optional) ", cxxopts::value<std::string>()->default_value("gun.stdhep"))
                     ("n,num_evt","Only process num_evt event [default:100000]", cxxopts::value<long>()->default_value("100000"))
@@ -64,18 +65,22 @@ int main(int argc, char **argv) {
     int ostream = 0;
     string outputfile = args["outputfile"].as<std::string>();
     long nevent = args["num_evt"].as<long>();
-    int  part_type = args["part"].as<int>();
-    double part_mass = args["mass"].as<double>();
-    double momentum_low = args["min"].as<double>(); // GeV
-    double momentum_max = args["max"].as<double>(); // GeV
+    int  part_types = args["part"].as<int>();
+    int  nmax = args["nmax"].as<int>();
+    double momentum_low = args["pmin"].as<double>(); // GeV
+    double momentum_max = args["pmax"].as<double>(); // GeV
     double x_min = args["xmin"].as<double>();     // cm @ 150 cm - a little inside the ECAL
     double x_max = args["xmax"].as<double>();     // cm @ 150 cm - a little inside the ECAL
     double y_min = args["ymin"].as<double>();      // cm @ 150 cm
     double y_max = args["ymax"].as<double>(); ;      // cm @ 150 cm
+    double rot = args["rot"].as<double>(); ;      // cm @ 150 cm
 
     open_write((char *)outputfile.c_str(),ostream,nevent);
 
+    vector<int> particle_ids = {   11,    -11,  22,  211,  -211,  13,  -13 }; // e- e+, gamma, pi+, pi-, mu-, mu+
+    vector<double> particle_mass = {0.000510998946, 0.000510998946, 0. , 0.13957039, 0.13957039, 0.1056583745, 0.1056583745};
 
+    if( part_types > particle_ids.size())  part_types = particle_ids.size();
     // Seed with a real random value, if available
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
@@ -83,34 +88,42 @@ int main(int argc, char **argv) {
     std::uniform_real_distribution<> x_dis(x_min,x_max);
     std::uniform_real_distribution<> y_dis(y_min,y_max);
     std::uniform_real_distribution<> one(0.,1.);
+    std::uniform_int_distribution<>  choose_num(1,nmax);
+    std::uniform_int_distribution<>  choose_part(0., part_types-1);
 
-    for(int i=0;i<nevent;++i){
+    for(int i=0; i < nevent; ++i){
 
         double momentum = momentum_dis(gen);
         double x        = x_dis(gen);
         double y        = y_dis(gen);
         if( one(gen) < 0.5) y = -y;
-        double r        = sqrt(x*x+y*y+150.*150.);
-        double theta    = acos(150./r);   // theta=arccos(z/r) rotated 32.5 mrad to +x
+        // double r        = sqrt(x*x+y*y+150.*150.);
+        double theta    = atan2(x,150.) + rot/1000.;  // theta=arccos(z/r) rotated 32.5 mrad to +x
         double phi      = atan2(y,x);
 
         evt.nevhep=i+1;
-        temp->isthep=1;  // Final state particle.
-        temp->idhep=part_type;  // PID - electron = 11
-        temp->jmohep[0]=0; // Position of mother particle in list.
-        temp->jmohep[1]=0; // Position of second mother particle in list.
-        temp->jdahep[0]=0; // Position of daughter particle in list.
-        temp->jdahep[1]=0;
-        temp->vhep[0]= 0; // Vertex-x;
-        temp->vhep[1]= 0; // Vertex-y;
-        temp->vhep[2]= 0; // Vertex-z;
-        temp->vhep[3]= 0; // Production time;
-        temp->phep[0]= momentum*sin(theta)*cos(phi); // Momentum-x;
-        temp->phep[1]= momentum*sin(theta)*sin(phi); // Momentum-y;
-        temp->phep[2]= momentum*cos(theta); // Momentum-z;
-        temp->phep[3]= sqrt(momentum*momentum+part_mass*part_mass); // Energy;
-        temp->phep[4]= part_mass; // Mass;
-        evt.particles.push_back(*temp);
+        int n_particles = choose_num(gen);
+        for( int n=0; n< n_particles; ++n){
+            int i_part = choose_part(gen);
+            double part_mass = particle_mass[i_part];
+            temp->isthep=1;  // Final state particle.
+            temp->idhep=particle_ids[i_part];  // PID - electron = 11
+            temp->jmohep[0]=0; // Position of mother particle in list.
+            temp->jmohep[1]=0; // Position of second mother particle in list.
+            temp->jdahep[0]=0; // Position of daughter particle in list.
+            temp->jdahep[1]=0;
+            temp->vhep[0]= 0; // Vertex-x;
+            temp->vhep[1]= 0; // Vertex-y;
+            temp->vhep[2]= 0; // Vertex-z;
+            temp->vhep[3]= 0; // Production time;
+            temp->phep[0]=  // Momentum-x;
+            temp->phep[1]= momentum*sin(theta)*sin(phi); // Momentum-y;
+            temp->phep[2]= momentum*cos(theta); // Momentum-z;
+            temp->phep[3]= sqrt(momentum*momentum+part_mass*part_mass); // Energy;
+            temp->phep[4]= part_mass; // Mass;
+            evt.particles.push_back(*temp);
+        }
+
         write_stdhep(&evt);
         write_file(ostream);
 
--- src/stdhep_print.cpp
+++ src/stdhep_print.cpp
@@ -4,70 +4,69 @@
 #include <string.h>
 #include "stdhep_util.hh"
 
-// takes input stdhep file, merges a fixed number of events, and writes to a new stdhep file
 int main(int argc,char** argv)
 {
-	stdhep_event new_event;
-	int istream = 0;
+    stdhep_event new_event;
+    int istream = 0;
 
-	if (argc<2 || argc>3)
-	{
-		printf("<input stdhep filename> [number of events to read]\n");
-		return 1;
-	}
-
-	int n_events = 0;
-	int n_read = 0;
-
-	n_events = open_read(argv[1],istream);
-
-	if (argc==3)
-	{
-		n_read = atoi(argv[2]);
-	}
-
-  printf("Total_z_momentum, e+e-_z_momentum\n");
-	for (int i=0;n_read==0||i<n_read;i++)
-	{
-		if (!read_next(istream)) {
-			close_read(istream);
-			return(0);
-		}
-
-		read_stdhep(&new_event);
-
-    if(new_event.particles.size() != 6){
-        printf("read event %d: nevhep = %d, nhep = %d\n",i+1,new_event.nevhep,new_event.particles.size());
-          if (new_event.has_hepev4)
-          {
-    //          printf("HEPEV4 information: idruplh = %d, eventweightlh = %E\n",new_event.idruplh,new_event.eventweightlh);//
-          }
-      
-        for (int j=0;j<new_event.particles.size();j++) {
-          print_entry(new_event.particles[j]);
-        }
-      
-      double p_sum_z=0;
-      double p_epem_sum_z = 0;
-      double p_elec = 0;
-      double p_pos = 0;
-      
-      for(int i=0;i<new_event.particles.size();++i){
-        if(new_event.particles[i].isthep == 1){
-          p_sum_z += new_event.particles[i].phep[2];
-          if(new_event.particles[i].idhep == -11){
-            if(new_event.particles[i].phep[2]> p_elec){
-              p_elec =new_event.particles[i].phep[2];
-            }
-          }else if(new_event.particles[i].idhep == 11){
-            p_pos =new_event.particles[i].phep[2];
-          }
-        }
-      }
-      printf("%10.6f, %10.6f \n",p_sum_z,p_elec+p_pos);
+    if (argc<2 || argc>3)
+    {
+        printf("<input stdhep filename> [number of events to read]\n");
+        return 1;
     }
-    
-    new_event.particles.clear();
-	}
+
+    int n_events = 0;
+    int n_read = 0;
+
+    n_events = open_read(argv[1],istream);
+
+    if (argc==3)
+    {
+        n_read = atoi(argv[2]);
+    }
+
+    printf("Total_z_momentum, e+e-_z_momentum\n");
+    for (int i=0;n_read==0||i<n_read;i++)
+    {
+        if (!read_next(istream)) {
+            close_read(istream);
+            return(0);
+        }
+
+        read_stdhep(&new_event);
+
+        if(new_event.particles.size() != 6){
+            printf("read event %d: nevhep = %d, nhep = %d\n",i+1,new_event.nevhep,new_event.particles.size());
+            if (new_event.has_hepev4)
+            {
+                //          printf("HEPEV4 information: idruplh = %d, eventweightlh = %E\n",new_event.idruplh,new_event.eventweightlh);//
+            }
+
+            for (int j=0;j<new_event.particles.size();j++) {
+                print_entry(new_event.particles[j]);
+            }
+
+            double p_sum_z=0;
+            double p_epem_sum_z = 0;
+            double p_elec = 0;
+            double p_pos = 0;
+
+            for(int i=0;i<new_event.particles.size();++i){
+                if(new_event.particles[i].isthep == 1){
+                    p_sum_z += new_event.particles[i].phep[2];
+                    if(new_event.particles[i].idhep == -11){
+                        if(new_event.particles[i].phep[2]> p_elec){
+                            p_elec =new_event.particles[i].phep[2];
+                        }
+                    }else if(new_event.particles[i].idhep == 11){
+                        p_pos =new_event.particles[i].phep[2];
+                    }
+                }
+            }
+            printf("%10.6f, %10.6f \n",p_sum_z,p_elec+p_pos);
+        }
+
+        new_event.particles.clear();
+    }
 }
 
